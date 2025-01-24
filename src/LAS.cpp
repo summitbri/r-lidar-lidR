a46468c186f57a9e04da257ec58070b9d895581a
summitbri <36425892+summitbri@users.noreply.github.com>
2025-01-24 10:52:13 -0700

    Update LAS.cpp

--- src/LAS.cpp
+++ src/LAS.cpp
@@ -619,7 +619,8 @@ void LAS::cut_overlap_with_grid(Rcpp::List layout, bool use_min) {
 
     double x = X[i];
     double y = Y[i];
-    double scan_angle = std::fabs(ScanAngleRank[i]); // Ensure fabs is used correctly for floating-point numbers
+    double scan_angle = std::fabs(data[i][13]); // Assuming column 14 is indexed as 13 (0-based index)
+    // double scan_angle = std::fabs(ScanAngleRank[i]); // Ensure fabs is used correctly for floating-point numbers
 
     // int col = std::floor((x - xmin) / xres);
     // int row = std::floor((ymax - y) / yres);
